67c7453b38b4259a0de629997b52b00378f2ab06
JJ Escandor <jj.p.escandor@gmail.com>
2022-08-19 15:13:28 -0400

    worked on merge-sort c-sharp

--- c-sharp/10_merge_sort.cs
+++ c-sharp/10_merge_sort.cs
@@ -5,7 +5,12 @@ class Program
     public static void Main(string[] args)
     {
         int[] unsorted = { 4, 5, 1, 2, 3, 0, 6 };
-        MergeSort(unsorted);
+        int[] sorted = MergeSort(unsorted);
+        Console.WriteLine("Sorted array");
+        foreach (int num in sorted)
+        {
+            Console.WriteLine($"{num}, ");
+        }
     }
     public static int[] MergeSort(int[] arr)
     {
@@ -13,19 +18,32 @@ class Program
         if (n > 1)
         {
             int mid = arr.Length / 2;
-            var left = arr.Take(mid);
-            foreach (var num in left)
-            {
-                Console.WriteLine(num);
-            }
+            int[] left = SliceArr(arr, 0, mid);
+            int[] right = SliceArr(arr, mid, arr.Length);
+            left = MergeSort(left);
+            right = MergeSort(right);
+            arr = Merge(left, right, arr);
         }
         return arr;
     }
-    public static T[] ArrayFromRange<T>(T[] originalArray, int startIndex, int length)
+
+    public static int[] Merge(int[] left, int[] right, int[] arr)
     {
-        int actualLength = Math.Min(length, originalArray.Length - startIndex);
-        T[] copy = new T[actualLength];
-        Array.Copy(originalArray, startIndex, copy, 0, actualLength);
-        return copy;
+        return arr;
+    }
+
+    public static int[] SliceArr(int[] arr, int startIndex, int length)
+    {
+        int[] newArr = new int[length - startIndex];
+        for (int i = 0; i < length; i++)
+        {
+            int idx = i + startIndex;
+            if (idx < arr.Length)
+            {
+                newArr[i] = arr[idx];
+            }
+
+        }
+        return newArr;
     }
 }
