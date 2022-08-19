8326e69571006a26e694829aab8c6b51da098dd3
JJ Escandor <jj.p.escandor@gmail.com>
2022-08-19 17:51:17 -0400

    started adding solutions to java directory

---
+++ java/01_contains_duplicate.java
@@ -0,0 +1,12 @@
+class HelloWorld
+{
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+
+    public static boolean containsDuplicate(int[] arr){
+        return False
+    }
+}
---
+++ java/10_merge_sort.java
@@ -0,0 +1,12 @@
+class HelloWorld
+{
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+
+    public static boolean mergeSort(int[] arr){
+        return arr
+    }
+}
