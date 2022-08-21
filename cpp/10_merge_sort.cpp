00effb37ae9a33a11cfa6b4d00f4b1f2127a56fe
JJ Escandor <jj.p.escandor@gmail.com>
2022-08-21 16:32:55 -0400

    worked on merge sort cpp

--- cpp/10_merge_sort.cpp
+++ cpp/10_merge_sort.cpp
@@ -1,39 +1,15 @@
-include <iostream>
-
+#include <iostream>
 using namespace std;
 
 
+void printArray(int A[], int size){
+    for(auto i = 0; i < size; i++)
+        cout << A[i] << " ";
+}
+
 int main(){
-    int[] unsortedInt = {4, 5, 1, 2, 3, 0, 6}
-
-}
-
-public int[] merge(int[] arr){
-    if (sizeof(arr) > 1){
-        int[] left = merget(arr);
-        int[] right = merge(arr);
-        arr = mergeSort(left, right, arr);
-    }
-    return arr
-}
-
-public int[] mergeSort(int[] left, int[] right, int[] arr){
-    int i, j, k;
-    i = j = k = 0;
-    while(i < sizeof(left) && j < sizeof(right)){
-        if (left[i] <= right[j]){
-            arr[k] = left[i];
-            i++;
-        }else{
-            arr[k] = right[j];
-            j++;
-        }
-        k++
-    }
-    if (i < sizeof(left)){
-        arr =  left;
-    }else{
-        arr = right;
-    }
-    return arr
+    int arr[] = {4, 5, 1, 2, 3, 0, 6};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    cout << "Give array is \n";
+    printArray(arr, arr_size);
 }
